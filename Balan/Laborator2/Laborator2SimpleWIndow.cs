2458885dcfa78486a45a90bf1b8b5dce2a244fcf
Victor Balan <victor.balan@assist.ro>
2024-01-15 00:17:53 +0200

    Laborator2_aplicatie

---
+++ Balan/Laborator2/Laborator2SimpleWIndow.cs
@@ -0,0 +1,208 @@
+﻿using OpenTK.Input;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
+
+namespace Laborator2
+{
+    class Laborator2SimpleWIndow : GameWindow
+    {
+        const float rotation_speed =180.0f;
+        float angle;
+        bool showCube = true;
+        KeyboardState lastKeyPress;
+        bool controlMouse = true;
+
+        public Laborator2SimpleWIndow() : base(800, 600)
+        {
+            KeyDown += Keyboard_KeyDown;
+        }
+
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F11)
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.Blue);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            //GL.MatrixMode(MatrixMode.Texture);
+            //GL.MatrixMode(MatrixMode.Modelview);
+            //GL.MatrixMode(MatrixMode.Color);
+            GL.LoadMatrix(ref perspective);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState keyboard = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
+
+            if (keyboard[Key.Escape])
+            {
+                Exit();
+                return;
+            }
+            else if (keyboard[Key.P] && !keyboard.Equals(lastKeyPress))
+            {
+                showCube = !showCube;
+            }
+            if(keyboard[Key.W])
+            {
+                controlMouse = !controlMouse;
+            }
+
+            // Keyboard control for rotation
+            if (keyboard[Key.A])
+            {
+                angle -= rotation_speed * (float)e.Time; // stanga
+            }
+            if (keyboard[Key.D])
+            {
+                angle += rotation_speed * (float)e.Time; // dreapta
+            }
+
+            // Mouse control
+            // Foloseste latimea ecranului pentru a calcula directia
+            if (controlMouse)
+            {
+                angle += (mouse.X - Width / 2) * 0.1f;
+            }
+            
+
+            lastKeyPress = keyboard;
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 lookat = Matrix4.LookAt(15, 50, 15, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            // Apply rotation
+            GL.Rotate(angle, 1.0f, 0.0f, 0.0f); // Rotate around the X axis
+            //GL.Rotate(angle, 0.0f, 1.0f, 0.0f); // Rotate around the Y axis
+            //GL.Rotate(angle, 0.0f, 0.0f, 1.0f); // Rotate around the Z axis
+
+
+            if (showCube)
+            {
+                DrawCube();
+                DrawAxes();
+            }
+
+            SwapBuffers();
+        }
+
+        private void DrawAxes()
+        {
+            GL.Begin(PrimitiveType.Lines);
+
+            // X
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(20, 0, 0);
+
+            // Y
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 20, 0);
+
+            // Z
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 20);
+
+
+            GL.End();
+        }
+
+        private void DrawCube()
+        {
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color3(Color.Silver);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color3(Color.Honeydew);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color3(Color.Moccasin);
+
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color3(Color.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (Laborator2SimpleWIndow app = new Laborator2SimpleWIndow())
+            {
+                app.Run(30.0, 0.0);
+            }
+        }
+    }
+}
